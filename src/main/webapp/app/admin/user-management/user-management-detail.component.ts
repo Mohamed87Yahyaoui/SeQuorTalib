ebf574e10f78bce3d3028f0f002f6131f239fe66
Mohamed yahyaoui <mohamedyahyaoui2000smia@gmail.com>
2020-07-19 18:50:41 +0100

    bouton supprmimer et modifier

--- src/main/webapp/app/admin/user-management/user-management-detail.component.ts
+++ src/main/webapp/app/admin/user-management/user-management-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
 import { User } from 'app/core/user/user.model';
+import { UserManagementDeleteDialogComponent } from './user-management-delete-dialog.component';
+import { Account } from 'app/core/user/account.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'jhi-user-mgmt-detail',
@@ -9,10 +11,16 @@ import { User } from 'app/core/user/user.model';
 })
 export class UserManagementDetailComponent implements OnInit {
   user: User | null = null;
+  currentAccount: Account | null = null;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private modalService: NgbModal) {}
 
   ngOnInit(): void {
     this.route.data.subscribe(({ user }) => (this.user = user));
+  }
+
+  deleteUser(user: User): void {
+    const modalRef = this.modalService.open(UserManagementDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
+    modalRef.componentInstance.user = user;
   }
 }
