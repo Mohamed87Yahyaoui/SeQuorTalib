78eb52bc606330c09e2fc852ab5baca2f0fb1e49
rachid rabou <rachidrabou4@gmail.com>
2020-07-30 02:44:10 +0200

    "login"

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { LoginService } from 'app/core/login/login.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrls: ['home.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('username', { static: false })
+  username?: ElementRef;
+
+  authenticationError = false;
+
+  loginForm = this.fb.group({
+    username: [''],
+    password: [''],
+    rememberMe: [false]
+  });
+
   account!: Account;
   authSubscription?: Subscription;
 
@@ -62,11 +75,38 @@ export class HomeComponent implements OnInit, OnDestroy {
   ];
   chartLabelsmohamed6 = ['2016', '2017', '2018', '2016'];
 
-  constructor(private accountService: AccountService, private loginModalService: LoginModalService, config: NgbCarouselConfig) {
+  constructor(
+    private fb: FormBuilder,
+    private loginService: LoginService,
+    private accountService: AccountService,
+    private loginModalService: LoginModalService,
+    config: NgbCarouselConfig
+  ) {
     config.interval = 2000;
     config.wrap = true;
     config.keyboard = false;
     config.pauseOnHover = false;
+  }
+
+  ngAfterViewInit(): void {
+    if (this.username) {
+      this.username.nativeElement.focus();
+    }
+  }
+
+  login(): void {
+    this.loginService
+      .login({
+        username: this.loginForm.get('username')!.value,
+        password: this.loginForm.get('password')!.value,
+        rememberMe: this.loginForm.get('rememberMe')!.value
+      })
+      .subscribe(
+        () => {
+          this.authenticationError = false;
+        },
+        () => (this.authenticationError = true)
+      );
   }
 
   ngOnInit(): void {
@@ -75,10 +115,6 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   isAuthenticated(): boolean {
     return this.accountService.isAuthenticated();
-  }
-
-  login(): void {
-    this.loginModalService.open();
   }
 
   ngOnDestroy(): void {
